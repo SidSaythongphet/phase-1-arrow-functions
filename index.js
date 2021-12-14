c9c4bcf9a663b62d486433fdb5bc45563d88db2b
Sid Saythongphet <sid.saythongphet@gmail.com>
2021-12-14 10:22:30 -0500

    commit tree f12a85918881cb77761e2ce4b3a3f8f50c1c9802

---
+++ index.js
@@ -0,0 +1,12 @@
+let parameter1 = 2000
+let parameter2 = 100
+const divide = function() {
+    return parameter1 / parameter2;
+}
+divide();
+
+const square = x => x * x;
+square(2);
+
+const add = (a, b) => a + b;
+add(3, 4);
